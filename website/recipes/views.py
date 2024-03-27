bc1454d6f972f329ae2deb8d9054338d1b473b14
Yehoon Lee <yehoon17@gmail.com>
2024-03-28 02:53:23 +0900

    Create view for homepage

--- website/recipes/views.py
+++ website/recipes/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Recipe, Category  # Import your models here
 
-# Create your views here.
+def homepage(request):
+    featured_recipes = Recipe.objects.all()[:3]  
+    categories = Category.objects.all() 
+    
+    context = {
+        'featured_recipes': featured_recipes,
+        'categories': categories,
+    }
+    
+    return render(request, 'recipes/homepage.html', context)
