50aee8c1ef614530ce48b6ebfbf9db33b5656a57
Yehoon Lee <yehoon17@gmail.com>
2024-03-28 04:17:15 +0900

    Create register
    `AttributeError at /register/Manager isn't available; 'auth.User' has been swapped for 'recipes.User'`

--- website/recipes/urls.py
+++ website/recipes/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
 ]
--- website/recipes/views.py
+++ website/recipes/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Recipe, Category
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import UserCreationForm
 
 def homepage(request):
     featured_recipes = Recipe.objects.all()[:3]  
@@ -13,5 +14,17 @@ def homepage(request):
     
     return render(request, 'recipes/homepage.html', context)
 
+
 class CustomLoginView(LoginView):
     template_name = 'recipes/login.html'
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Redirect to login page after successful registration
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
