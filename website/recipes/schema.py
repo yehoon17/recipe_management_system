eae4fbd5873fa7db2b0ec12f4fce7f5f036d8929
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2024-03-29 06:34:35 +0900

    Create GraphQL mutations

--- website/recipes/schema.py
+++ website/recipes/schema.py
@@ -1,6 +1,7 @@
 import graphene
 from graphene_django.types import DjangoObjectType
 from .models import Recipe, Ingredient
+from .mutations import CreateRecipeMutation
 
 class RecipeType(DjangoObjectType):
     class Meta:
@@ -12,12 +13,21 @@ class IngredientType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     all_recipes = graphene.List(RecipeType)
-    all_ingredients = graphene.List(IngredientType)
-
     def resolve_all_recipes(root, info):
         return Recipe.objects.all()
 
+    all_ingredients = graphene.List(IngredientType)
     def resolve_all_ingredients(root, info):
         return Ingredient.objects.all()
+    
+    recipe_by_id = graphene.Field(RecipeType, id=graphene.Int())
+    def resolve_recipe_by_id(self, info, id):
+        return Recipe.objects.get(id=id)
 
-schema = graphene.Schema(query=Query)
+# Define Mutation class with mutation fields
+class Mutation(graphene.ObjectType):
+    # Include mutation classes from mutations.py
+    create_recipe = CreateRecipeMutation.Field()
+
+# Create Schema
+schema = graphene.Schema(query=Query, mutation=Mutation)
