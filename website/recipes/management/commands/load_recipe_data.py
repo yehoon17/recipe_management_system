d7166023209e57df486f9d64ddebde77fe9756d5
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2024-03-30 04:51:10 +0900

    Implement uploading image files

--- website/recipes/management/commands/load_recipe_data.py
+++ website/recipes/management/commands/load_recipe_data.py
@@ -1,3 +1,4 @@
+import os
 import json
 import random
 from django.core.management.base import BaseCommand
@@ -23,11 +24,30 @@ class Command(BaseCommand):
                 user = random.choice(users)
                 
                 new_recipe = Recipe.objects.create(user=user, **recipe)
+                self.load_image(new_recipe)
+
                 self.create_recipe_ingredient(new_recipe, ingredients)
                 self.create_recipe_tag(new_recipe, tags)
 
                 self.stdout.write(self.style.SUCCESS(f"Created recipe with ID: {new_recipe.id}"))
 
+
+    def load_image(self, recipe):
+        image_name = f"{recipe.title.replace(' ', '_')}.jpg"
+        image_path = os.path.join('../data/images/', image_name)
+
+        # Check if the image file exists
+        if os.path.exists(image_path):
+            # Open and read the image file
+            with open(image_path, 'rb') as img_file:
+                # Assign the image file to the image field of the new recipe
+                recipe.image.save(image_name, img_file, save=True)
+
+            # Print message when image is found
+            self.stdout.write(self.style.SUCCESS(f"Image found for recipe: {recipe.title}"))
+        else:
+            # Print message when image is not found
+            self.stdout.write(self.style.WARNING(f"No image found for recipe: {recipe.title}"))
 
     def create_recipe_ingredient(self, recipe, ingredients):
         for ingredient in ingredients:
